dba7c91a8512f89248adfa8bccef4e233957c48d
Lvin Xu <lvin_xu@outlook.com>
2022-03-11 14:53:11 +0800

    add utils

--- packages/renderer/src/components/asideApiList/AsideApiList.vue
+++ packages/renderer/src/components/asideApiList/AsideApiList.vue
@@ -10,20 +10,30 @@
             </el-input>
         </div>
         <div class="max-h-[600px] overflow-auto">
+            <h1>{{ msg }}</h1>
             <h1 class="text-left mt-[10px]">API列表</h1>
             <div
                 class="scrollbar-demo-item border border-black-100 w-[580px] hover:shadow-md mt-[10px] mb-[10px]"
-                v-for="item in 20"
+                v-for="item in props.testNumber.sec.number"
                 :key="item"
-            >{{ `API:${item},Method:${Math.random() < 0.5 ? 'GET' : 'POST'}` }}</div>
+            >{{ `API:${item}` }}</div>
         </div>
     </div>
 </template>
 
 <script setup lang='ts'>
-import { ref } from 'vue';
-import { Search } from '@element-plus/icons-vue'
-const searchInput = ref('')
+import { ref, toRefs } from 'vue';
+import { Search } from '@element-plus/icons-vue';
+const searchInput = ref('');
+const props = defineProps<{
+    testNumber: {
+        sec: {
+            number: number,
+        },
+        msg: string,
+    }
+}>()
+const { msg } = toRefs(props.testNumber)
 </script>
 
 <style lang='less' scoped>
